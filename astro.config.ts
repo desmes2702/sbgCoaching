561b26d484fcf59b58b610eb4bc6d965e6844027
desmes <antoine.thiri@gmail.com>
2025-06-04 20:16:24 +0200

    build website --> dist

--- astro.config.ts
+++ astro.config.ts
@@ -1,16 +1,17 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import netlify from '@astrojs/netlify'; // ✅ Adapter ajouté
 import viteConfig from './vite.config.ts';
 
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
-// ✅ recrée __dirname pour le contexte ESM
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
   integrations: [react()],
+  adapter: netlify(), // ✅ Ajout ici
   vite: {
     ...viteConfig,
   },
