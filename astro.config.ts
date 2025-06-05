8b1e5ec81d20afaffe83c366c7ed4304aeafcc26
desmes <antoine.thiri@gmail.com>
2025-06-05 12:28:39 +0200

    Config Astro: Vercel adapter

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import node from '@astrojs/node'; // ✅
+import vercel from '@astrojs/vercel';
 import viteConfig from './vite.config.ts';
 
 export default defineConfig({
   integrations: [react()],
-  adapter: node({ mode: 'standalone' }), // ✅ Build static clean
-  output: 'static',
+  adapter: vercel(),
   vite: {
     ...viteConfig,
   },
